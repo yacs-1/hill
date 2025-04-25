bd701a451e4deec3704ee76cf80b058c01fe287f
yacs-1 <chromebook1@youngsvilleacademy.org>
2025-04-24 21:50:32 -0400

    Update global-header.js

--- global-header.js
+++ global-header.js
@@ -4,6 +4,6 @@ This script will inject the global page header into the #header div of each page
 
 const = INJECTION = '<div id="global-preheader"><h1>Web Development</h1></div><div id="global-header"><ul id="nav-list"><li><a href="https://yacs-1.github.io/hill/">Home</a></li><li><a href="/hill/photos/index.html>Photos</a></li><ul><li><a href="#">Shopping</a></li><li><a href="#">Art</a></li></ul><li><a href="/hill/web-dev"></a>Web Development</a></li></ul></div>  ';
 
-let_div = document.getElementById("global-header");
+let_div = document.getElementById("gheader");
 
 _div.innerHTML = INJECTION;
