ada7dcd330ad479264f91ac22cb36e7ea9ec7667
yacs-1 <chromebook1@youngsvilleacademy.org>
2025-05-27 15:16:46 -0400

    Update timer.js

--- wdsub/timer.js
+++ wdsub/timer.js
@@ -1,26 +1,26 @@
 let initDate;
-let timeDifference = 0;
+let timeDiff = 0;
 let hasInitDate = false;
 
 cookieArr = document.cookie.split(";");
 
 for (const element of cookieArr) {
-  if(element.split("=")[0] == "init-data"){
-    hasInitDate = true;
-    initDate = new Date(element.split("=")[1]);
-  }
+	if (element.split("=")[0] == "init-date") {
+		hasInitDate = true;
+		initDate = new Date(element.split("=")[1]);
+	}
 }
 
 if (!hasInitDate) {
-  document.cookie = "init-date=" + new Date() + "; max-age=31536000000";
-} else{
-  let now = new Date();
-  document.cookie = "new-date=" + now + "; max-age=31536000000";
-  timeDifference = now.getTime() - initDate.getTime();//gets the difference in milliseconds from first access until now
+	document.cookie = "init-date=" + new Date() + "; max-age=31536000000";
+} else {
+	let now = new Date();
+	document.cookie = "new-date=" + now + "; max-age=31536000000";
+	timeDiff = now.getTime() - initDate.getTime(); //get difference in the milliseconds from first access until now
 }
 
 let timeSpent = document.getElementById("time-spent");
-let numHours = Math.floor(timeDifference / 360000);
-let numMins = Math.floor((timeDifference % 360000) * 60);
+let numHours = Math.floor(timeDiff / 360000);
+let numMins = Math.floor((timeDiff % 360000) * 60);
 let diffStr = numHours + ":" + numMins;
-timeSpent.innerText = diffStr;
+timeSpent.innerHTML = diffStr;
